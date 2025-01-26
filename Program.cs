3f6cb335175bf1016901239a4c4144c803508572
CayqueSiqueira <cayquesm@gmail.com>
2025-01-26 19:47:48 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Start(6);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+            while(currentTime != time)
+            {   
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine("Time: " + currentTime);
+                Thread.Sleep(1000); // garante que o programa vai esperar 1 segundo
+                //é preciso importar a biblioteca System.Threading
+            }
+
+                Console.Clear();
+                Console.WriteLine("Stopwatch finalizado");
+        }
+    }
+}
